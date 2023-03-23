17a794286584e9125d962fe7a19ab803d6297eac
khoind <khoind@rikkeisoft.com>
2023-03-23 16:41:36 +0700

    update code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,42 @@
 <template>
-    <div class="flex justify-between bg-[#f8f8f8] h-screen">
-        <div class="hidden md:block">
-            <TheSidebar />
-        </div>
-        <div class="flex-grow flex flex-col h-screen overflow-x-hidden">
-            <TheHeader />
-            <div class="flex-grow overflow-y-auto max-h-screen custom-scroll">
-                <nuxt class="bg-[#FAFBFF] shadow-light p-4 m-3 md:m-6 overflow-x-hidden" />
-            </div>
-            <TheFooter />
-        </div>
+  <div class="min-h-screen flex flex-col">
+    <Header class="sticky top-0 w-full z-40" />
+    <Banner class="absolute top-0" />
+    <div class="absolute z-50 top-1/4 left-1/2 -translate-x-1/2 text-center">
+      <h1
+        class="text-7xl outline-4 text-white shadow-2xl font-bold mb-1"
+        style="font-family: roboto"
+      >
+        BẠN ĐANG MUỐN ĐI ĐÂU?
+      </h1>
+      <span
+        class="outline-4 text-white shadow-2xl text-2xl"
+        style="font-family: roboto"
+        >Hãy để Cattour tư vấn cho bạn chuyến đi tuyệt vời nhất!</span
+      >
     </div>
+
+    <div
+      class="container mx-auto max-w-[1200px] flex-grow my-4 xl:my-8 px-4 xl:px-0"
+    >
+      <nuxt v-if="!$slots.default" />
+      <slot />
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
-    import TheSidebar from '@/components/layout/TheSidebar.vue';
-    import TheHeader from '@/components/layout/TheHeader.vue';
-    import TheFooter from '@/components/layout/TheFooter.vue';
+import Header from "@/components/layout/TheHeader.vue";
+import Footer from "@/components/layout/TheFooter.vue";
+import Banner from "@/components/banner/Banner.vue";
 
-    export default {
-        components: {
-            TheSidebar,
-            TheHeader,
-            TheFooter,
-        },
-    };
+export default {
+  components: {
+    Header,
+    Footer,
+    Banner,
+  },
+};
 </script>
+
