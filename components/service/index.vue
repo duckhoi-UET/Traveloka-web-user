cc5a6af6019317a1de0194d3aee9e04b69200d55
KhoiND <khoinguyen2000bg@gmail.com>
2023-05-28 14:38:53 +0700

    Fix bug

--- components/service/index.vue
+++ components/service/index.vue
@@ -27,16 +27,21 @@
     </div>
     <div class="mt-4">
       <h3 class="text-3xl capitalize">Ẩm thực</h3>
-      <div
-        class="grid grid-cols-3 gap-4 mb-6"
-        v-for="(item, index) in foots"
-        :key="index"
-      >
-        <div class="w-[350px] h-[250px] shadow-lg">
-          <img class="w-full h-full rounded-sm" :src="item.image" alt="foots" />
+      <div v-for="(item, index) in foots" :key="index">
+        <div class="max-w-[900px] mx-auto underline">
+          <h3 class="text-xl italic">{{ item.title }}</h3>
         </div>
-        <div class="col-span-2">
-          <span class="text-lg font-medium">{{ item.desc }}</span>
+        <div class="flex flex-col items-center mb-6">
+          <div class="w-[450px] h-[350px] shadow-lg">
+            <img
+              class="w-full h-full rounded-sm"
+              :src="item.image"
+              alt="foots"
+            />
+          </div>
+          <div class="col-span-2 max-w-[900px] mt-3">
+            <span class="text-lg font-medium">{{ item.desc }}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -63,10 +68,12 @@ export default {
       ],
       foots: [
         {
+          title: "Thịt dê",
           desc: "Được chăn thả bên trên những ngọn núi đá nên thịt dê ở Ninh Bình vô cùng săn chắc và dai nên du khách đến Ninh Bình nhất định phải thử món ăn này một lần. Trong khi thịt dê ở đây còn được chế biến ra nhiều món ăn ngon với những cách chế biến không giống nhau như làm thành nem dê, dê hấp, dê nướng hay nổi tiếng nhất là dê tương gừng - một món ăn cực kỳ giàu dinh dưỡng. Nhiều người cũng bảo rằng thịt dê còn có thể chữa được rất nhiều loại bệnh, nên đến Ninh Bình nếu có cơ hội bạn chớ bỏ lỡ thịt dê núi nức tiếng nơi đây.",
           image: "/images/foot/image7.png",
         },
         {
+          title: "Cơm cháy",
           desc: "Nhắc đến đặc sản Ninh Bình không thể bỏ qua món cơm cháy ruốc thơm ngon. Điều làm nên sự đặc biệt của món cơm cháy này là người chế biến phải thực hiện vô cùng tỉ mỉ trong từng khâu thực hiện. Cơm cháy phải làm từ những loại gạo ngon và đem nếu vừa phải tới khi chín giòn đúng chuẩn.",
           image: "/images/foot/image2.png",
         },
